187c36ab4b6e0b4db7c2eba0fabe299a565cb4d4
PatrykZymelka <patrykzymelka@op.pl>
2023-11-13 17:26:33 +0100

    Front Page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
-
-import './App.css'
+import logo from "./images/logo.svg";
+import triangle from "./images/bg-triangle.svg";
+import rock from "./images/icon-rock.svg"
+import paper from "./images/icon-paper.svg"
+import scissors from "./images/icon-scissors.svg"
+import './App.css';
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className="Page">
+      <div className="Top">
+        <img src={logo} alt="logo" className="Logo"/>
+        <div className="Score">Score</div>
+      </div>
+      <div className="Game">
+        <img src={triangle} alt="triangle" className="Triangle"/>
+        <div className="Rock">
+          <img src={rock} alt="rock"/>
+        </div>
+        <div className="Paper">
+          <img src={paper} alt="paper" />
+        </div>
+        <div className="Scissors">
+          <img src={scissors} alt="scissors" />
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
