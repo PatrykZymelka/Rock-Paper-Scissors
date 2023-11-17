d99a383cd6d8702e7f7ee0d4e9b6bd9b60f5014d
PatrykZymelka <127216593+PatrykZymelka@users.noreply.github.com>
2023-11-17 12:07:52 +0100

    Update: Cleaning up

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
 import logo from "./images/logo.svg";
 import triangle from "./images/bg-triangle.svg";
-import rock from "./images/icon-rock.svg"
-import paper from "./images/icon-paper.svg"
-import scissors from "./images/icon-scissors.svg"
-import './App.css';
+import rock from "./images/icon-rock.svg";
+import paper from "./images/icon-paper.svg";
+import scissors from "./images/icon-scissors.svg";
+import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  
-  const [chosen, setChosen] = useState()
-  const [phase, setPhase] = useState()
+  const [chosen, setChosen] = useState();
+  const [phase, setPhase] = useState();
 
-  useEffect(()=>{
-    function status(phase){
-    document.getElementById("one").style.display = "inline"
-    return () => {
-      document.getElementById("one").style.display = "none"
+  useEffect(() => {
+    function status(phase) {
+      document.getElementById("two").style.display = "inline";
+      return () => {
+        document.getElementById("one").style.display = "none";
+      };
     }
-    }
-  },[chosen])
+  }, [chosen]);
 
   return (
     <div className="Page">
       <div className="Top">
-        <img src={logo} alt="logo" className="Logo"/>
+        <img src={logo} alt="logo" className="Logo" />
         <div className="Score">Score</div>
       </div>
       <div className="Game">
         <div className="One" id="one">
-          <img src={triangle} alt="triangle" className="Triangle"/>
+          <img src={triangle} alt="triangle" className="Triangle" />
           <div className="Rock" onClick={() => setChosen("Rock")}>
-            <img src={rock} alt="rock"/>
+            <img src={rock} alt="rock" />
           </div>
           <div className="Paper" onClick={() => setChosen("Paper")}>
             <img src={paper} alt="paper" />
@@ -38,14 +37,11 @@ function App() {
           <div className="Scissors" onClick={() => setChosen("Scissors")}>
             <img src={scissors} alt="scissors" />
           </div>
-          <div>
-            You have chosen: {chosen}
-          </div>
+          <div>You have chosen: {chosen}</div>
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
